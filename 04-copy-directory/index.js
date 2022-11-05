fe5e4782c61286d280d5c489584aa039ddb653cf
V-NV <96265492+V-NV@users.noreply.github.com>
2022-11-05 15:46:01 +0300

    feat:add solution 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+const fsP = fs.promises;
 
+const Path__From = path.join(__dirname, 'files');
+const Path__To = path.join(__dirname, 'files-copy');
+
+async function fc(x){
+await fsP.rmdir(x, { recursive: true }, (err) => { 
+ if (err) throw err;
+}).then(()=>{
+
+
+fsP.mkdir(Path__To, { recursive: true }, (err) => {
+  if (err) {throw err}
+});}).then(()=>{
+
+fs.readdir(Path__From, { withFileTypes: true }, (err, files) => {
+  if (err) {throw err}
+  else{
+  files.forEach((file) => {
+    if (file.isFile()) {
+     fs.copyFile(path.join(Path__From, file.name),path.join(Path__To, file.name),(err)=> {
+      if (err) {throw err}
+    })
+  }
+  })}
+})})
+}
+fc(Path__To);
